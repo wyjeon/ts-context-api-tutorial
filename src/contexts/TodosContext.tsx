dd70104063dd3273171738697984733764539832
wyjeon <wyjeon.info@gmail.com>
2021-03-04 13:51:14 +0900

    TypeScript Context API

---
+++ src/contexts/TodosContext.tsx
@@ -0,0 +1,89 @@
+import { createContext, Dispatch, useContext, useReducer } from 'react';
+
+// 나중에 다른 컴포넌트에서 타입을 불러와서 쓸 수 있도록 내보내겠습니다.
+export type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+// 상태전용 Context 만들기
+type TodoState = Todo[];
+const TodosStateContext = createContext<TodoState | undefined>(undefined);
+
+// 액션을 위한 타입 선언하기
+type Action =
+  | { type: 'CREATE'; text: string }
+  | { type: 'TOGGLE'; id: number }
+  | { type: 'REMOVE'; id: number };
+
+// dispatch 전용 Context 만들기
+type TodosDispatch = Dispatch<Action>;
+const TodosDispatchContext = createContext<TodosDispatch | undefined>(
+  undefined
+);
+
+// 리듀서 작성하기
+function todosReducer(state: TodoState, action: Action): TodoState {
+  switch (action.type) {
+    case 'CREATE':
+      const nextId = Math.max(...state.map(todo => todo.id)) + 1;
+      return state.concat({
+        id: nextId,
+        text: action.text,
+        done: false,
+      });
+    case 'TOGGLE':
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+// TodosProvider 만들기
+export function TodosContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [todos, dispatch] = useReducer(todosReducer, [
+    {
+      id: 1,
+      text: 'Context API 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: 'TypeScript 배우기',
+      done: true,
+    },
+    {
+      id: 3,
+      text: 'TypeScript 와 Context API 함께 사용하기',
+      done: false,
+    },
+  ]);
+  return (
+    <TodosDispatchContext.Provider value={dispatch}>
+      <TodosStateContext.Provider value={todos}>
+        {children}
+      </TodosStateContext.Provider>
+    </TodosDispatchContext.Provider>
+  );
+}
+
+export function useTodosState() {
+  const state = useContext(TodosStateContext);
+  if (!state) throw new Error('TodosProvider not found');
+  return state;
+}
+
+export function useTodosDispatch() {
+  const dispatch = useContext(TodosDispatchContext);
+  if (!dispatch) throw new Error('TodosProvider not found');
+  return dispatch;
+}
